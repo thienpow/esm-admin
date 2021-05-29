7819bb898498833832a79673d7df0d0cca0558f1
Ken Chong <thienpow@gmail.com>
2021-05-29 18:01:56 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 yarn build
+
+# clean up www inside deploy/admin folder
 rm -rf ~/development/ESM/BackEnd/esm-datastore/deploy/admin/www
+# copy over new www
 cp -r www ~/development/ESM/BackEnd/esm-datastore/deploy/admin/
-cd ~/development/ESM/BackEnd/esm-datastore/
+# goto the deploy folder
+cd ~/development/ESM/BackEnd/esm-datastore/deploy
 git add .
 git commit -m "esm-admin"
 git push
-ssh root@esm-api.aadi.my 'bash -s' < restart_esm_admin.sh
+ssh root@esm-api.aadi.my 'bash -s' < remote_deploy_esm_admin.sh
